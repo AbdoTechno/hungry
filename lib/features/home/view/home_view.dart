c2e8d0829ba5b088f21dbdce28e4b2ae80802dd9
AbdoTechno <abdoalsenaweabdo@gmail.com>
2026-05-26 16:16:20 +0300

    test

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -11,7 +11,9 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
+      onTap: () => FocusScope.of(context).unfocus(), 
+
+  
       child: Scaffold(
         body: SafeArea(
           child: Padding(
